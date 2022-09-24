3c9ccfad3fc57e913f6c4ef11f57eced7cb711f9
ivoncone <posdatamexico@gmail.com>
2022-09-24 16:36:11 -0700

    input

--- brain.py
+++ brain.py
@@ -10,7 +10,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -20,19 +20,20 @@ documents = []
 ignore_letters = ['?', '!', '.', ',', '*', '-']
 
 for intent in intents['intents']:
-	for patterns in intent['patterns']:
-		word_list = nltk.tokenize(pattern)
-		words.append(word_list)
-		documents.append((word_list), intent['tag'])
-		if intents['tag'] not in classes:
+	for pattern in intent['patterns']:
+		word_list = nltk.word_tokenize(pattern)
+		words.extend(word_list)
+		documents.append((word_list, intent['tag']))
+		if intent['tag'] not in classes:
 			classes.append(intent['tag'])
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
 
 classes = sorted(set(classes))
-pickle.dump(wors, open('words.pk1', 'wb'))
-pickle.dump(words, open('classes.pk1', 'wb'))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empy = [0] * len(classes)
@@ -40,7 +41,7 @@ output_empy = [0] * len(classes)
 for document in documents:
 	bag = []
 	word_patterns = document[0]
-	word_patterns = [lemmatizer.lemmatize(word.lower() for word in word_patterns)]
+	word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
 	for word in words:
 		bag.append(1) if word in word_patterns else bag.append(0)
 
@@ -64,6 +65,6 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd= SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.model')
-prnt("done") 
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print("done") 
